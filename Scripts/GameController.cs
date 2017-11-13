577f133b11de3adc8408fa4603ef78eb68e65a52
Tomek <tomasz.kubit@performgroup.com>
2017-11-13 19:44:28 +0100

    First commit

---
+++ Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    public Text[,] buttonList;
+
+    private void Awake()
+    {
+        SetGameControllerReferenceOnButtons();
+    }
+
+    void SetGameControllerReferenceOnButtons()
+    {
+        for (int i = 0; i < buttonList.GetLength(0); i++)
+        {
+            for (int j = 0; j < buttonList.GetLength(1); j++)
+            {
+                buttonList[i,j].GetComponentInParent<GridSpace>().SetGameControllerReference(this);
+            }
+        }
+    }
+
+    public void EndTurn()
+    {
+        Debug.Log("ENnn");
+    }
+
+}
---
+++ Scripts/GridSpace.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridSpace : MonoBehaviour {
+
+    public Button button;
+    public Text buttonText;
+
+    private int[,] numbers;
+    private int[,] numbersLeft;
+
+    private GameController gameController;
+
+    private void Awake()
+    {
+        numbers = new int[3, 3];
+        numbersLeft = numbers;
+    }
+
+    public void SetSpace()
+    {
+
+    }
+
+    public void SetGameControllerReference(GameController controller)
+    {
+        gameController = controller;
+    }
+
+}
