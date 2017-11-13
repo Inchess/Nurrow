2eaf974f7057eab47f6bd53aff8ec3174986d2c0
Tomek <tomasz.kubit@performgroup.com>
2017-11-13 20:36:33 +0100

    Prepare method that changes 1D array into 2D array

--- Scripts/GameController.cs
+++ Scripts/GameController.cs
@@ -11,17 +11,42 @@ public class GameController : MonoBehaviour {
     private void Awake()
     {
         SetGameControllerReferenceOnButtons();
+        Change1DTo2DArray();
+        AddText();
     }
 
     void SetGameControllerReferenceOnButtons()
     {
-        for (int i = 0; i < buttonList.GetLength(0); i++)
+        for (int i = 0; i < buttonList.Length; i++)
         {
-            for (int j = 0; j < buttonList.GetLength(1); j++)
-            {
-                buttonList[i].GetComponentInParent<GridSpace>().SetGameControllerReference(this);
-            }
+            buttonList[i].GetComponentInParent<GridSpace>().SetGameControllerReference(this);
         }
+    }
+
+    void Change1DTo2DArray()
+    {
+        buttonArray[2, 0] = buttonList[0];
+        buttonArray[2, 1] = buttonList[1];
+        buttonArray[2, 2] = buttonList[2];
+        buttonArray[1, 0] = buttonList[3];
+        buttonArray[1, 1] = buttonList[4];
+        buttonArray[1, 2] = buttonList[5];
+        buttonArray[0, 0] = buttonList[6];
+        buttonArray[0, 1] = buttonList[7];
+        buttonArray[0, 2] = buttonList[8];
+    }
+
+    void AddText()
+    {
+        buttonArray[2, 0].text = "7";
+        buttonArray[2, 1].text = "8";
+        buttonArray[2, 2].text = "9";
+        buttonArray[1, 0].text = "6";
+        buttonArray[1, 1].text = "5";
+        buttonArray[1, 2].text = "4";
+        buttonArray[0, 0].text = "1";
+        buttonArray[0, 1].text = "2";
+        buttonArray[0, 2].text = "3";
     }
 
     public void EndTurn()
