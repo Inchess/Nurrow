65bdd3de26a55796e3f72d52974e2d0f94037d00
Tomek <tomasz.kubit@performgroup.com>
2017-11-17 10:49:00 +0100

    Game is working!

--- Scripts/GameController.cs
+++ Scripts/GameController.cs
@@ -18,6 +18,7 @@ public class GameController : MonoBehaviour {
     private int rows = 3;
     private int columns = 3;
     private Text[,] buttonArray;
+    private Image[,] imageArray;
     private List<int> numbers;
     private List<int> numbersLeft;
     private System.Random rand;
@@ -32,6 +33,12 @@ public class GameController : MonoBehaviour {
     string downArrowName = "Down Arrow";
     string downRightArrowName = "Down Right Arrow";
     string downLeftArrowName = "Down Left Arrow";
+    private int clickedButtonX;
+    private int clickedButtonY;
+    private string clickedButtonArrowName;
+    private int newButtonX;
+    private int newButtonY;
+    private string newButtonNumber;
 
 
     //          BOARD
@@ -62,6 +69,7 @@ public class GameController : MonoBehaviour {
     private void InstantiateVariables()
     {
         buttonArray = new Text[rows, columns];
+        imageArray = new Image[rows, columns];
         numbers = new List<int>(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
         numbersLeft = numbers;
         arrowsList = new List<Sprite>(new Sprite[] { upArrow, upLeftArrow, upRightArrow, leftArrow, rightArrow, downArrow, downLeftArrow, downRightArrow });
@@ -93,14 +101,23 @@ public class GameController : MonoBehaviour {
     void Change1DTo2DArray()
     {
         buttonArray[2, 0] = buttonList[0];
+        imageArray[2, 0] = imageList[0];
         buttonArray[2, 1] = buttonList[1];
+        imageArray[2, 1] = imageList[1];
         buttonArray[2, 2] = buttonList[2];
+        imageArray[2, 2] = imageList[2];
         buttonArray[1, 0] = buttonList[3];
+        imageArray[1, 0] = imageList[3];
         buttonArray[1, 1] = buttonList[4];
+        imageArray[1, 1] = imageList[4];
         buttonArray[1, 2] = buttonList[5];
+        imageArray[1, 2] = imageList[5];
         buttonArray[0, 0] = buttonList[6];
+        imageArray[0, 0] = imageList[6];
         buttonArray[0, 1] = buttonList[7];
+        imageArray[0, 1] = imageList[7];
         buttonArray[0, 2] = buttonList[8];
+        imageArray[0, 2] = imageList[8];
     }
 
     void PrepareArrowsToUse()
@@ -139,50 +156,52 @@ public class GameController : MonoBehaviour {
 
     void AddArrowsToButtons()
     {
-        for (int i = 0; i < imageList.Length; i++)
+        for (int i = 0; i < rows; i++)
         {
-            int index = rand.Next(0, arrowsToUse.Count);
-            imageList[i].sprite = arrowsToUse[index];
-            imageList[i].rectTransform.sizeDelta = new Vector2(40, 40);
-            SetArrowLocation(arrowsToUse[index], imageList[i]);
-            arrowsToUse.RemoveAt(index);
+            for (int j = 0; j < columns; j++)
+            {
+                int index = rand.Next(0, arrowsToUse.Count);
+                imageArray[i, j].sprite = arrowsToUse[index];
+                imageArray[i, j].rectTransform.sizeDelta = new Vector2(40, 40);
+                SetArrowLocation(arrowsToUse[index], imageArray[i, j]);
+                arrowsToUse.RemoveAt(index);
+            }
         }
     }
 
     void SetArrowLocation(Sprite arrow, Image image)
     {
-        string name = arrow.name;
         int x = 0;
         int y = 0;
-        if (name == upArrowName)
+        if (arrow.name == upArrowName)
         {
             x = 0;
             y = 45;
-        } else if (name == upRightArrowName)
+        } else if (arrow.name == upRightArrowName)
         {
             x = 45;
             y = 45;
-        } else if (name == upLeftArrowName)
+        } else if (arrow.name == upLeftArrowName)
         {
             x = -45;
             y = 45;
-        } else if (name == rightArrowName)
+        } else if (arrow.name == rightArrowName)
         {
             x = 45;
             y = 0;
-        } else if (name == leftArrowName)
+        } else if (arrow.name == leftArrowName)
         {
             x = -45;
             y = 0;
-        } else if (name == downArrowName)
+        } else if (arrow.name == downArrowName)
         {
             x = 0;
             y = -45;
-        } else if (name == downLeftArrowName)
+        } else if (arrow.name == downLeftArrowName)
         {
             x = -45;
             y = -45;
-        } else if (name == downRightArrowName)
+        } else if (arrow.name == downRightArrowName)
         {
             x = 45;
             y = -45;
@@ -199,6 +218,7 @@ public class GameController : MonoBehaviour {
     {
         string number = button.GetComponentInChildren<Text>().text;
         FindButtonIndexes(number);
+        ChangeButtonsPlaces(number);
     }
 
     void FindButtonIndexes(string number)
@@ -209,10 +229,76 @@ public class GameController : MonoBehaviour {
             {
                 if (buttonArray[i, j].text == number)
                 {
-                    Debug.Log("i: " + i + ", j: " + j);
+                    clickedButtonArrowName = imageArray[i, j].sprite.name;
+                    clickedButtonX = i;
+                    clickedButtonY = j;
                 }
             }
         }
     }
 
+    void ChangeButtonsPlaces(string clickedButtonNumber)
+    {
+        CalculateNewLocation();
+        string oldButtonNumber = clickedButtonNumber;
+        int oldButtonX = clickedButtonX;
+        int oldButtonY = clickedButtonY;
+        buttonArray[newButtonX, newButtonY].text = clickedButtonNumber;
+        buttonArray[clickedButtonX, clickedButtonY].text = newButtonNumber;
+        
+
+    }
+
+    void CalculateNewLocation()
+    {
+        int xChange = 0;
+        int yChange = 0;
+        if (clickedButtonArrowName == upArrowName)
+        {
+            xChange = 1;
+            yChange = 0;
+        }
+        else if (clickedButtonArrowName == upRightArrowName)
+        {
+            xChange = 1;
+            yChange = 1;
+        }
+        else if (clickedButtonArrowName == upLeftArrowName)
+        {
+            xChange = 1;
+            yChange = -1;
+        }
+        else if (clickedButtonArrowName == rightArrowName)
+        {
+            xChange = 0;
+            yChange = 1;
+        }
+        else if (clickedButtonArrowName == leftArrowName)
+        {
+            xChange = 0;
+            yChange = -1;
+        }
+        else if (clickedButtonArrowName == downArrowName)
+        {
+            xChange = -1;
+            yChange = 0;
+        }
+        else if (clickedButtonArrowName == downLeftArrowName)
+        {
+            xChange = -1;
+            yChange = -1;
+        }
+        else if (clickedButtonArrowName == downRightArrowName)
+        {
+            xChange = -1;
+            yChange = 1;
+        }
+
+
+        newButtonX = (clickedButtonX + xChange + rows) % rows;
+        newButtonY = (clickedButtonY + yChange + rows) % rows;
+        newButtonNumber = buttonArray[newButtonX, newButtonY].text;
+        Debug.Log("newX: " + newButtonX + ", newY: " + newButtonY + ", newNumber: " + newButtonNumber);
+
+    }
 }
